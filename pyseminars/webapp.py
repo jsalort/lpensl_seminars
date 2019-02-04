8e33ac78dd100deaf47fdec956787847ca7dbb7d
Julien Salort <julien@salort.eu>
2019-02-04 17:14:33 +0100

    Add web interface

--- pyseminars/webapp.py
+++ pyseminars/webapp.py
@@ -4,14 +4,19 @@ Flask app serving the calendar
 
 """
 
-from flask import Flask, Response, abort
-from pyseminars.feeds import feeds
+import os
+from flask import Flask, Response, abort, render_template, redirect, url_for
+from flask_moment import Moment
+from pyseminars.feeds import feeds, now
 
-app = Flask(__name__)
-
+root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+app = Flask(__name__,
+            template_folder=os.path.join(root, 'templates'),
+            static_folder=os.path.join(root, 'static'))
+moment = Moment(app)
 
 @app.route("/<feed_name>.ics")
-def main(feed_name):
+def calendar(feed_name):
     for f in feeds:
         if f.feed_name == feed_name:
             break
@@ -19,6 +24,29 @@ def main(feed_name):
         return abort(404)
     c = f.generate_calendar()
     return Response(str(c), mimetype='text/calendar')
+
+
+@app.route("/<feed_name>")
+def feed(feed_name):
+    for f in feeds:
+        if f.feed_name == feed_name:
+            break
+    else:
+        return abort(404)
+    c = f.generate_calendar()
+    th = now()
+    past_events = {e for e in c.events if e.end < th and e.description}
+    upcoming_events = {e for e in c.events if e.end >= th and e.description}
+    return render_template('main.html',
+                           feeds=feeds,
+                           feed_name=feed_name,
+                           past_events=sorted(past_events, key=lambda e: e.begin),
+                           upcoming_events=sorted(upcoming_events, key=lambda e: e.begin))
+
+
+@app.route("/")
+def main():
+    return redirect(url_for('feed', feed_name=feeds[0]))
 
 
 if __name__ == '__main__':
